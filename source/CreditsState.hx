b2960010acace97327414dec087a059775ca517a
TheGuyWhoPorts <121072594+TheGuyWhoPorts@users.noreply.github.com>
2023-01-17 22:17:08 -0500

    Add files via upload

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -81,7 +81,8 @@ class CreditsState extends MusicBeatState
 		#end
 
 		var pisspoop:Array<Array<String>> = [ //Name - Icon name - Description - Link - BG Color
-
+                        ['Android Apk Port'],
+                        ['OswaldGamerYT', 'yt', 'Hi I Port This Mod Android Port With Source Code', 'https://youtube.com/@OswaldGamerYT840', 'FFFFFF']
 			['Psych Engine Android Port'],
                         ['idklool (or Boyfriend FNF idc)', 'nothing', 'Hi I ported the Psych Engine', 'youtube.com/c/BoyfriendFNF', 'FFFFFF'],
                         ['Dxgamer', 'dxgamer', 'Coder Help', 'https://youtube.com/c/Dxgamer7405', 'FFFFFF'],
@@ -304,8 +305,8 @@ class CreditsState extends MusicBeatState
 		if(modsAdded.contains(folder)) return;
 
 		var creditsFile:String = null;
-		if(folder != null && folder.trim().length > 0) creditsFile = Paths.assets(folder + '/data/credits.txt');
-		else creditsFile = Paths.assets('data/credits.txt');
+		if(folder != null && folder.trim().length > 0) creditsFile = Paths.mods(folder + '/data/credits.txt');
+		else creditsFile = Paths.mods('data/credits.txt');
 
 		if (Assets.exists(creditsFile))
 		{
